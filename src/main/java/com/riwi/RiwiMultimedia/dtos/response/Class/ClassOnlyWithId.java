34d047eb4e2d5eecc63ed51aaac1318fbc141c31
samuelcoder1 <echeverrysamuel74@gmail.com>
2024-08-30 21:38:05 -0500

    Se realiza el metodo post y ReadByPages

---
+++ src/main/java/com/riwi/RiwiMultimedia/dtos/response/Class/ClassOnlyWithId.java
@@ -0,0 +1,12 @@
+package com.riwi.RiwiMultimedia.dtos.response.Class;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClassOnlyWithId {
+    private Integer id;
+}
