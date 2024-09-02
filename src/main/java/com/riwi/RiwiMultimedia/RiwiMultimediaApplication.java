67459f7982cf3083b03309fa283c1c36970c5df0
San7imo <santiago.londono07@gmail.com>
2024-09-02 16:39:44 -0500

    Se realiza lesson

--- src/main/java/com/riwi/RiwiMultimedia/RiwiMultimediaApplication.java
+++ src/main/java/com/riwi/RiwiMultimedia/RiwiMultimediaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RiwiMultimediaApplication {
+public class taRiwiMultimediaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RiwiMultimediaApplication.class, args);
